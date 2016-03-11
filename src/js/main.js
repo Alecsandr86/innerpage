93119715c80aeed973679880293f3a07688fcf5d
Alecsandr86 <alecsandrcherneckiy@gmail.com>
2016-03-12 00:01:25 +0200

    first commit 1200

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@
  Third party
  */
 //= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/jquery-form-styler/jquery.formstyler.min.js
+//= ../../bower_components/jquery.easing/js/jquery.easing.min.js
+//= ../../bower_components/waypoints/lib/jquery.waypoints.min.js
 
 /*
     Custom
--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -1,10 +1,93 @@
-//Так приятней :)
-window.log = function(param){
-    console.log(param);
-};
+
 $(document).ready(function(){
-    $('.icon').bind('click',function(){
-        alert('Все работает!!! :)')
-    })
+
+    //Chrome Smooth Scroll
+    try {
+        $.browserSelector();
+        if($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch(err) {
+
+    };
+
+
+
+    $('select').styler();
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    var animationName='flipInY';
+
+    $('.animate .item').bind('mouseenter',function(){
+        $(this).find("img").animate({
+            width:'110%',
+            marginLeft:'-5%',
+            marginTop:'-10%',
+            marginBottom:'-10%'
+        },300);
+        $(this).find(".hovers").animate({
+            opacity: 1
+        },300);
+        $(this).find('.plai-big').addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+
+    });
+    $('.animate .item').bind('mouseleave',function(){
+        $(this).find("img").animate({
+            width:'100%',
+            marginLeft:'0',
+            marginTop:'0',
+            marginBottom:'0'
+        },300);
+
+        $(this).find(".hovers").animate({
+            opacity: 0
+        },300);
+    });
+
+
+    /*******************************
+     *   ПЛАВНЫЙ ПЕРЕХОД ПО ЯКОРЯМ
+     *******************************/
+    $('a[href^="#link"]').click(function(){
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top},2000,"easeInOutExpo");
+        return false;
+    });
+
+
+    /*******************************
+     *          WAYPOINTS
+     *******************************/
+
+    $('.podcast .item')
+        .animate({'opacity':'0'})
+        .waypoint(function(dire){
+            if (dire === 'down') {
+                $(this.element).animate({'opacity':'1'}).addClass('animated fadeInLeft').one(animationEnd, function() {
+                    $(this.element).removeClass('animated fadeInLeft');
+                });
+            }
+
+        }, {
+            offset: '90%'
+        });
+
+
+    /************************
+     *       preloader
+     ************************/
+    $(window).on('load', function () {
+        var $preloader = $('#loading'),
+            $spinner   = $preloader.find('#loading-center');
+        $spinner.fadeOut();
+        $preloader.fadeOut('slow');
+    });
+
+    /************************
+     *       /preloader/
+     ************************/
+
 
 });
